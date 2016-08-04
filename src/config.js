bde572e05cca892b566e4987e2cf19335b84a884
Davide Bianchi <davide.bianchi@mondora.com>
2016-08-04 11:23:45 +0200

    Added dotenv

--- src/config.js
+++ src/config.js
@@ -1,3 +1,7 @@
+import dotenv from "dotenv";
+
+dotenv.load();
+
 export const MONGODB_URL = process.env.MONGODB_URL || "mongodb://localhost:27017/test";
 export const USERS_COLLECTION_NAME = "users";
 export const SURVEY = "survey";
