0d2bf14032be68cb1babfd3631b36a4d97a10db3
Davide Bianchi <davide.bianchi@mondora.com>
2016-08-04 18:22:27 +0200

    Update test and added log

--- src/steps/upsert-completed-survey.js
+++ src/steps/upsert-completed-survey.js
@@ -1,6 +1,7 @@
 import {equals, filter} from "ramda";
 
 import {USERS_COLLECTION_NAME} from "../config";
+import log from "../services/logger";
 import {upsert} from "../services/mongodb";
 
 export default async function upsertCompletedSurvey (reading, user) {
@@ -9,5 +10,6 @@ export default async function upsertCompletedSurvey (reading, user) {
     // if the selected survey is already in surveys array, remove it
     const filteredSurveys = filter(userSurvey => !equals(userSurvey.id, surveyId), userSurveys);
     const surveys = filteredSurveys.concat([{id: surveyId, completed: true}]);
+    log.info(surveys);
     return await upsert(USERS_COLLECTION_NAME, {surveys}, user._id);
 }
--- test/utils.js
+++ test/utils.js
@@ -10,29 +10,35 @@ export function getAnswers (type="survey", category="survey") {
                 "answers": [{
                     "id": 1,
                     "timestamp": "2016-08-01T11:01:44.408Z",
-                    "answer": "answer 1"
+                    "answer": "answer 1",
+                    "questions": {
+                        "text": "Question 1",
+                        "category": "category"
+                    }
                 }, {
                     "id": 2,
                     "timestamp": "2016-08-01T11:01:44.708Z",
-                    "answer": "answer 2"
+                    "answer": "answer 2",
+                    "questions": {
+                        "text": "Question 2",
+                        "category": "category"
+                    }
                 }, {
                     "id": 3,
                     "timestamp": "2016-08-01T11:01:45.008Z",
-                    "answer": "answer 3"
+                    "answer": "answer 3",
+                    "questions": {
+                        "text": "Question 3",
+                        "category": "category"
+                    }
                 }, {
                     "id": 4,
                     "timestamp": "2016-08-01T11:01:45.808Z",
-                    "answer": "answer 4"
-                }],
-                "questions": [{
-                    "id": 1,
-                    "text": "Question 1"
-                }, {
-                    "id": 2,
-                    "text": "Question 2"
-                }, {
-                    "id": 3,
-                    "text": "Question 3"
+                    "answer": "answer 4",
+                    "questions": {
+                        "text": "Question 4",
+                        "category": "category"
+                    }
                 }]
             },
             "id": "answer"
